44fb135d767a9e70b808b74583722e37356609f8
Tashri2342 <108603052+Tashri2342@users.noreply.github.com>
2023-06-29 17:49:55 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 13965888
-api_hash = "5f2f806877d357ba957b7e5e1fc95a3e"
-bot_token = "6064809764:AAGgyWLJCXEe5GpYN4HusxEWbCeE_uqVv3w"
+api_id = 1359658688566
+api_hash = "5f2f806877d356567g7ba957b7e5e1fc95a3e"
+bot_token = "6067884809764:AAGgyWLJCXEe5GpYN4HusxEWbCeE_uqVv3w"
 sudo_groups = -1001824573993
